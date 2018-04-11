ec459ab468315c7a0bc4b5c76bbb2327a40cbc23
quentingnt <quentin.guenot@viacesi.fr>
2018-04-11 13:39:45 +0200

    Merge branch 'master' of https://github.com/JulienHenrotin/Project-WEB

--- projetWEB/resources/views/inscription.blade.php
+++
@@ -1,9 +0,0 @@
-<form method="post">
-    {{ csrf_field() }}
-    <input type="text" name="prenom" placeholder="Prénom">
-    <input type="text" name="nom" placeholder="Nom">
-    <input type="mail" name="mail" placeholder="Mail">
-    <input type="password" name="password" placeholder="Mot de Passe">
-    <input type="password" name="password_confirmation" placeholder="Confirmation Mot de Passe">
-    <input type="submit" value="Inscription">
-</form>
