083042d209f0b6c8546b1d677c3ac5d53fb27667
JulienHenrotin <JulienHenrotin@users.noreply.github.com>
2018-04-11 10:49:27 +0200

    page inscription Julien

---
+++ projetWEB/app/Http/Controllers/inscriptionControleur.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class inscriptionControleur extends Controller
+{
+    public function formulaire()
+    {
+        return view('inscription');
+    }
+
+    public function traitement()
+    {
+        request()->validate([
+            'prenom' =>['required'],
+            'nom' =>['required'],
+            'mail'=>['email','required'],
+            'password'=>['required','confirmed'],
+            'password_confirmation'=>['required'],
+        ]);
+        //placement dans la base de donnée
+        $utilisateurs = new \App\utilisateurs();
+        $utilisateurs->prenom = request('prenom');
+        $utilisateurs->nom = request('nom');
+        $utilisateurs->mail = request('mail');
+        $utilisateurs->MDP = request('password');
+        $utilisateurs->id_statut ='1';
+        $utilisateurs->save();
+    }
+}
