b1689a45f9c50c0805b477e9befeda5fb4de66d2
JulienHenrotin <JulienHenrotin@users.noreply.github.com>
2018-04-11 14:00:28 +0200

    modif Julien

---
+++ projetWEB/resources/views/connexion.blade.php
@@ -0,0 +1,6 @@
+<form method="post">
+    {{ csrf_field() }}
+    <input type="mail" name="mail" placeholder="Mail">
+    <input type="password" name="password" placeholder="Mot de Passe">
+    <input type="submit" value="Connexion">
+</form>
