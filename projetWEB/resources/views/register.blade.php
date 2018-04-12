d4bec585d35be5a93442d6ff528ef625bd01ccb0
quentingnt <quentin.guenot@viacesi.fr>
2018-04-12 14:27:30 +0200

    Merge branch 'master' of https://github.com/JulienHenrotin/Project-WEB

---
+++ projetWEB/resources/views/register.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Inscription</title>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+    <link href="css/style.css" rel='stylesheet' type='text/css' />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <script src="js/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(".dropdown img.flag").addClass("flagvisibility");
+
+            $(".dropdown dt a").click(function() {
+                $(".dropdown dd ul").toggle();
+            });
+
+            $(".dropdown dd ul li a").click(function() {
+                var text = $(this).html();
+                $(".dropdown dt a span").html(text);
+                $(".dropdown dd ul").hide();
+                $("#result").html("Selected value is: " + getSelectedValue("sample"));
+            });
+
+            function getSelectedValue(id) {
+                return $("#" + id).find("dt a span.value").html();
+            }
+
+            $(document).bind('click', function(e) {
+                var $clicked = $(e.target);
+                if (! $clicked.parents().hasClass("dropdown"))
+                    $(".dropdown dd ul").hide();
+            });
+
+
+            $("#flagSwitcher").click(function() {
+                $(".dropdown img.flag").toggleClass("flagvisibility");
+            });
+        });
+    </script>
+</head>
+
+
+<?php include ('menu.blade.html'); ?>
+
+<div class="main">
+    <div class="shop_top">
+        <div class="container">
+            <form>
+                <div class="register-top-grid">
+                    <h3>INFORMATIONS PERSONNELLES</h3>
+                    <div>
+                        <span>Prénom<label>*</label></span>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <span>Nom<label>*</label></span>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <span>Adresse email<label>*</label></span>
+                        <input type="text">
+                    </div>
+                    <div class="clear"> </div>
+                    <a class="news-letter" href="#">
+                        <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>S'inscrire à la Newsletter</label>
+                    </a>
+                    <div class="clear"> </div>
+                </div>
+                <div class="clear"> </div>
+                <div class="register-bottom-grid">
+                    <h3>INFORMATION DE CONNEXION</h3>
+                    <div>
+                        <span>Mot de passe<label>*</label></span>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <span>Confirmation du mot de passe<label>*</label></span>
+                        <input type="text">
+                    </div>
+                    <div class="clear"> </div>
+                </div>
+                <div class="clear"> </div>
+                <input type="submit" value="Confirmer">
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<?php include ('footer.blade.html'); ?>
--- projetWEB/routes/web.php
+++ projetWEB/routes/web.php
@@ -34,6 +34,14 @@ Route::get('/contact', function() {
     return view('contact');
 });
 
+Route::get('/login', function() {
+    return view('login');
+});
+
+Route::get('/register', function() {
+    return view('register');
+});
+
 
 Route::get('/experiance', function() {
     return view('experience');
