2fe63c93c043e1d86173319bdce9324b6ee59652
quentingnt <quentin.guenot@viacesi.fr>
2018-04-12 15:16:38 +0200

    Merge branch 'master' of https://github.com/JulienHenrotin/Project-WEB

--- projetWEB/resources/views/contact.blade.php
+++ projetWEB/resources/views/contact.blade.php
@@ -67,19 +67,18 @@ License URL: http://creativecommons.org/licenses/by/3.0/
             <div class="row">
                 <div class="col-md-7">
                     <div class="map">
-                        <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.in/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265&amp;output=embed"></iframe><br><small><a href="https://maps.google.co.in/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265" style="color:#666;text-align:left;font-size:12px"></a></small>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2635.5907187582807!2d6.128592415828599!3d48.655944422893285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794a23f4fa43a27%3A0x86a779438fb342d!2s14+All%C3%A9e+de+la+For%C3%AAt+de+la+Reine%2C+54500+Villers-l%C3%A8s-Nancy!5e0!3m2!1sfr!2sfr!4v1523536743027" width="600" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
                 </div>
                 <div class="col-md-5">
-                    <p class="m_8">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</p>
+                    <p class="m_8">ma vie</p>
                     <div class="address">
-                        <p>500 Lorem Ipsum Dolor Sit,</p>
-                        <p>22-56-2-9 Sit Amet, Lorem,</p>
-                        <p>USA</p>
-                        <p>Phone:(00) 222 666 444</p>
-                        <p>Fax: (000) 000 00 00 0</p>
-                        <p>Email: <span>support[at]snow.com</span></p>
-                        <p>Follow on: <span>Facebook</span>, <span>Twitter</span></p>
+                        <p>14-16 Allée de la Forêt de la Reine</p>
+                        <p>54500 Villers-lès-Nancy</p>
+                        <p>FRANCE</p>
+                        <p>Téléphone : 03 12 34 56 78</p>
+                        <p>Email: <span>exianancy.bde@gmail.com</span></p>
+                        <p>Nos réseaux : <a href="https://www.facebook.com/EXiaNancyBde/"><span>Facebook</span></a>, <a href="https://twitter.com/BdeExiaNancy"><span>Twitter</span></a></p>
                     </div>
                 </div>
             </div>
@@ -87,14 +86,14 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                 <div class="col-md-12 contact">
                     <form method="post" action="contact-post.html">
                         <div class="to">
-                            <input type="text" class="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}">
+                            <input type="text" class="text" value="Nom" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}">
                             <input type="text" class="text" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}">
-                            <input type="text" class="text" value="Subject" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Subject';}">
+                            <input type="text" class="text" value="Objet" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Subject';}">
                         </div>
                         <div class="text">
-                            <textarea value="Message:" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message';}">Message:</textarea>
+                            <textarea value="Message :" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message';}">Message:</textarea>
                             <div class="form-submit">
-                                <input name="submit" type="submit" id="submit" value="Submit"><br>
+                                <input name="submit" type="submit" id="submit" value="Envoyer"><br>
                             </div>
                         </div>
                         <div class="clear"></div>
