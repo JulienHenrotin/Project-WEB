ea2a10e487af8fea63ff84b928a2256c43bd78b6
JulienHenrotin <JulienHenrotin@users.noreply.github.com>
2018-04-12 08:44:59 +0200

    jeudi

--- projetWEB/resources/views/admin.blade.php
+++ projetWEB/resources/views/admin.blade.php
@@ -1,7 +1,7 @@
 <form action="http://localhost/projetWEB/Project-WEB/projetWEB/public/admin" method="post">
     {{ csrf_field() }}
-    <input type="submit" value="Evenements" >
-    <input type="submit" value="ajouterGoodies">
-    <input type="submit" value="ajouterEvent">
-    <input type="submit" value="BAI" >
+    <input type="submit" name="Evenements" value="Evenements" >
+    <input type="submit" name="ajouterGoodies" value="ajouterGoodies">
+    <input type="submit" name="ajouterEvent" value="ajouterEvent">
+    <input type="submit" name="BAI" value="BAI" >
 </form>
--- projetWEB/resources/views/connexion.balde.php
+++
@@ -1,6 +0,0 @@
-<form method="post">
-    {{ csrf_field() }}
-    <input type="mail" name="mail" placeholder="Mail">
-    <input type="password" name="password" placeholder="Mot de Passe">
-    <input type="submit" value="Connexion">
-</form>
